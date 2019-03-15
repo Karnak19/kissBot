7009a760ffb91fdbfcb87e027d5b598d9cc9d492
Basile <basile64.v@gmail.com>
2019-03-15 11:12:26 +0100

    remove magicka role from roles.js file

--- commands/roles.js
+++ commands/roles.js
@@ -16,15 +16,6 @@ module.exports = class RoleCreation {
             message.member.addRole(role);
             message.reply(`T'as le rôle ${role} gros`);
          }
-      } else if (args[0] === "magicka") {
-         let role = message.guild.roles.find("name", "Magicka");
-         if (message.member.roles.find("name", "Magicka")) {
-            message.member.removeRole(role);
-            message.reply(`Dommage ! t'es plus ${role}`);
-         } else {
-            message.member.addRole(role);
-            message.reply(`T'as le rôle ${role} gros`);
-         }
       } else {
          message.reply("Eh ça existe pas ce rôle");
       }
