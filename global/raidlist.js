327540cbdca8cac6afc3459d434dd584a37c76e7
Basile <basile64.v@gmail.com>
2019-05-25 17:26:38 +0200

    added husky w/ pre-commit hooks, and sunspire in reaidlist

--- global/raidlist.js
+++ global/raidlist.js
@@ -1,36 +1,63 @@
-module.exports =  [{
+module.exports = [
+  {
     label: "cr",
     name: "Cloudrest",
-    url:'https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0',
-    image:'https://images.uesp.net/thumb/2/24/ON-prerelease-Cloudrest_02.jpg/800px-ON-prerelease-Cloudrest_02.jpg'
-},{
+    url:
+      "https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0",
+    image:
+      "https://images.uesp.net/thumb/2/24/ON-prerelease-Cloudrest_02.jpg/800px-ON-prerelease-Cloudrest_02.jpg"
+  },
+  {
     label: "hrc",
     name: "Hel Ra Citadel",
-    url:'https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0',
-    image:'https://images.uesp.net/thumb/5/5a/ON-interior-Hel_Ra_Citadel_03.jpg/800px-ON-interior-Hel_Ra_Citadel_03.jpg'
-},{
+    url:
+      "https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0",
+    image:
+      "https://images.uesp.net/thumb/5/5a/ON-interior-Hel_Ra_Citadel_03.jpg/800px-ON-interior-Hel_Ra_Citadel_03.jpg"
+  },
+  {
     label: "so",
     name: "Sanctum Ophidia",
-    url:'https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0',
-    image:'https://images.uesp.net/thumb/2/21/ON-interior-Sanctum_Ophidia.jpg/800px-ON-interior-Sanctum_Ophidia.jpg'
-},{
+    url:
+      "https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0",
+    image:
+      "https://images.uesp.net/thumb/2/21/ON-interior-Sanctum_Ophidia.jpg/800px-ON-interior-Sanctum_Ophidia.jpg"
+  },
+  {
     label: "as",
     name: "Asylum Sanctorium",
-    url:'https://docs.google.com/spreadsheets/d/1YaeF9jxRWwQush7FINSGH5kd7v9A3yp4dp7rhqHy6n0/edit#gid=0',
-    image:'https://esosslfiles-a.akamaihd.net/cms/2017/10/8703be03dabbac6811cf1e8970e2e415.jpg'
-},{
+    url:
+      "https://docs.google.com/spreadsheets/d/1YaeF9jxRWwQush7FINSGH5kd7v9A3yp4dp7rhqHy6n0/edit#gid=0",
+    image:
+      "https://esosslfiles-a.akamaihd.net/cms/2017/10/8703be03dabbac6811cf1e8970e2e415.jpg"
+  },
+  {
     label: "aa",
     name: "Aetherian Archive",
-    url:'https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0',
-    image:'https://latenighteso.com/wp-content/uploads/2017/04/aetherian-archive-guide-1024x400.jpg'
-},{
+    url:
+      "https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0",
+    image:
+      "https://latenighteso.com/wp-content/uploads/2017/04/aetherian-archive-guide-1024x400.jpg"
+  },
+  {
     label: "hof",
     name: "Hall of Fabrication",
-    url:'https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0',
-    image:'http://teso-delve.com/gfx/dungeon/halls-of-fabrication.jpg'
-},{
+    url:
+      "https://docs.google.com/spreadsheets/d/1JK4OqSud938J39PPbz6zfLLFyR6_lN1tayxB9hwBjZ8/edit#gid=0",
+    image: "http://teso-delve.com/gfx/dungeon/halls-of-fabrication.jpg"
+  },
+  {
     label: "mol",
     name: "Maw of Lorkaj",
-    url:'https://docs.google.com/spreadsheets/d/1CJZY5VOl0mz8PE91gdf5b1l8KygUrg3A6CfSwPjBcHs/edit#gid=0',
-    image:'https://en.uesp.net/wiki/File:ON-place-The_High_Lunarium.jpg'
-}];
+    url:
+      "https://docs.google.com/spreadsheets/d/1CJZY5VOl0mz8PE91gdf5b1l8KygUrg3A6CfSwPjBcHs/edit#gid=0",
+    image: "https://en.uesp.net/wiki/File:ON-place-The_High_Lunarium.jpg"
+  },
+  {
+    label: "ss",
+    name: "Sunspire",
+    url:
+      "https://docs.google.com/spreadsheets/d/1j1qixqL06rANuES_tjwpXPnitBqYutsyDiX5kWap1AE/edit?usp=sharing",
+    image: "https://en.uesp.net/wiki/File:ON-place-The_High_Lunarium.jpg"
+  }
+];
